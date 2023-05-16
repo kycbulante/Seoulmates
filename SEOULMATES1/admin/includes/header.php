8878cd84b9a7966eb06fb06b97380cf52bc75744
kycbulante <92903243+kycbulante@users.noreply.github.com>
2023-05-16 11:35:23 +0800

    Add files via upload

---
+++ SEOULMATES1/admin/includes/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <title>AllCategories</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="AllCategories.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 5.3.2, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+</head>
+    <title>
+    Seoulmates
+  </title>
+      Fonts and icons    
+  
+  <style>
+    .form-control{
+      border: 1px solid #b3a1a1 !important;
+      padding: 8px 10px;
+    }
+    .form-select{
+      border: 1px solid #b3a1a1 !important;
+      padding: 8px 10px;
+    }
+    
+  </style>
+</head>
+
+<body class="g-sidenav-show  bg-gray-200">
+    <?php include('sidebar.php'); ?>
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+        <?php include('navbar.php');?>
---
+++ SEOULMATES1/includes/header.php
@@ -0,0 +1,25 @@
+<?php 
+// session_start();
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/custom.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Seoulmates</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <?php include('navbar.php'); ?>
